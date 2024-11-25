b9cd05680e762b5482e59a6592acf1ad2e1e7e58
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2024-11-25 19:54:04 +0530

    Added assets + Footer Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/global/Footer";
 import NavBar from "./components/global/NavBar";
 
 function App() {
   return (
     <div className="bg-[#071624] h-screen">
       <NavBar />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/global/Footer.tsx
@@ -0,0 +1,94 @@
+import Logo from "../../assets/footer-logo.svg";
+import { FaRedditAlien } from "react-icons/fa6";
+import { FaTelegram } from "react-icons/fa";
+import { FiTwitter } from "react-icons/fi";
+import classNames from "classnames";
+import { useCallback, useEffect, useState } from "react";
+
+function Footer() {
+  const [hash, setHash] = useState<string>("home");
+
+  const setHashFunction = useCallback(
+    () => setHash(window.location.href.split("#")[1] || ""),
+    []
+  );
+
+  useEffect(() => {
+    setHashFunction();
+    window.addEventListener("hashchange", setHashFunction);
+    return () => window.removeEventListener("hashchange", setHashFunction);
+  }, [setHashFunction]);
+
+  return (
+    <div className="flex justify-around text-white font-raleway font-extrabold">
+      <div>
+        <img src={Logo} alt="logo" />
+      </div>
+      <div className="flex items-center gap-16">
+        <a
+          href="#about"
+          className={classNames(
+            "hover:text-primary transition-color duration-300",
+            { "text-primary": hash === "about" }
+          )}
+        >
+          About Us
+        </a>
+        <a
+          href="#roadmap"
+          className={classNames(
+            "hover:text-primary transition-color duration-300",
+            { "text-primary": hash === "roadmap" }
+          )}
+        >
+          Roadmap
+        </a>
+        <a
+          href="#faq"
+          className={classNames(
+            "hover:text-primary transition-color duration-300",
+            { "text-primary": hash === "faq" }
+          )}
+        >
+          FAQs
+        </a>
+        <a
+          href="#contact"
+          className={classNames(
+            "hover:text-primary transition-color duration-300",
+            { "text-primary": hash === "contact" }
+          )}
+        >
+          Contact Us
+        </a>
+      </div>
+      <div className="text-2xl">
+        <div className="my-4">
+          Contact <span className="text-primary">Us</span>
+        </div>
+        <div className="flex items-center gap-6 justify-center">
+          <a
+            href="#telegram"
+            className="hover:text-primary transition-color duration-300"
+          >
+            <FaTelegram />
+          </a>
+          <a
+            href="#reddit"
+            className="hover:text-primary transition-color duration-300"
+          >
+            <FaRedditAlien />
+          </a>
+          <a
+            href="#twitter"
+            className="hover:text-primary transition-color duration-300"
+          >
+            <FiTwitter />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
